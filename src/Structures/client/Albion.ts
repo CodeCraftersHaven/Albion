f38ad5d82e8bc2ebc06c4b9d07264f3c827ae845
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-07-17 15:15:13 -0500

    remove unnecessary client properties

--- src/Structures/client/Albion.ts
+++ src/Structures/client/Albion.ts
@@ -4,35 +4,9 @@ import { Client, GatewayIntentBits, Partials } from 'discord.js';
 export class Albion extends Client {
   constructor(private cooldowns: Cooldowns) {
     super({
-      intents: [
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.DirectMessageTyping,
-        GatewayIntentBits.GuildInvites,
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildEmojisAndStickers,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.GuildWebhooks,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMessageTyping,
-        GatewayIntentBits.GuildModeration,
-        GatewayIntentBits.GuildScheduledEvents
-      ],
-      partials: [
-        Partials.Channel,
-        Partials.GuildMember,
-        Partials.GuildScheduledEvent,
-        Partials.Message,
-        Partials.Reaction,
-        Partials.ThreadMember,
-        Partials.User
-      ],
+      intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.Guilds],
       allowedMentions: {
-        repliedUser: false
-        // roles: ["1231658494139174912"],
+        repliedUser: false //Do not mention user in reply!
       },
       shards: 'auto'
     });
