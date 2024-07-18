027ff13c03aeae595253b6734bfb20c7cefb60b1
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-07-17 22:22:22 -0500

    remove placeholders

--- src/commands/components/buttons/!placeholder.ts
+++
@@ -1,9 +0,0 @@
-import { commandModule, CommandType } from '@sern/handler';
-
-export default commandModule({
-  type: CommandType.Button,
-  async execute(
-    interaction
-    //,sdt
-  ) {}
-});
