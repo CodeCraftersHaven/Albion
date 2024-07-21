d07e918b7ba3adddd3bfd1aa644f0614caa20bce
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-07-20 22:02:45 -0500

    create default publish config for commands

--- src/Structures/sern/sIndex.ts
+++ src/Structures/sern/sIndex.ts
@@ -4,17 +4,4 @@ export * from './plugins/cooldown.js';
 export * from './plugins/permCheck.js';
 export * from './plugins/subcommandPermCheck.js';
 
-export enum IntegrationContextType {
-  GUILD = 0,
-  BOT_DM = 1,
-  PRIVATE_CHANNEL = 2
-}
-
-export const allContexts: NonEmptyArray<IntegrationContextType> = [
-  IntegrationContextType.BOT_DM,
-  IntegrationContextType.GUILD,
-  IntegrationContextType.PRIVATE_CHANNEL
-];
-type NonEmptyArray<T> = [T, ...T[]];
-
-type ContextType = IntegrationContextType | 0 | 1 | 2;
+export const defaultPublishConfig = { contexts: [0, 1, 2], integrationTypes: [0, 1] };
