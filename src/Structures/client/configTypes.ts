0e1faf0ef11438709c0849b9e55506e0e625881d
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-07-27 10:01:03 -0500

    add channels to typings

--- src/Structures/client/configTypes.ts
+++ src/Structures/client/configTypes.ts
@@ -1,4 +1,9 @@
 export type ChannelIds = {
   main_guild_id: string;
-  channel_ids: { 'emoji-submissions': string, 'bot-logs': string };
+  channel_ids: {
+    'emoji-submissions': string;
+    'bot-logs': string;
+    'suggestions': string;
+    'testing': string;
+  };
 };
