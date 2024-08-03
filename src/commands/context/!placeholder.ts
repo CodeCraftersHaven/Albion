973e951d881c46be5cc726bc686a529eb69f2e9c
Peter-MJ-Parker <34216187+Peter-MJ-Parker@users.noreply.github.com>
2024-08-03 14:49:18 -0500

    delete placeholder

--- src/commands/context/!placeholder.ts
+++
@@ -1,9 +0,0 @@
-import { commandModule, CommandType } from '@sern/handler';
-
-export default commandModule({
-  type: CommandType.CtxMsg,
-  async execute(
-    interaction
-    //,sdt
-  ) {}
-});
